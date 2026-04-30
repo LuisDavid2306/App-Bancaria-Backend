16df3ada879030e7438d6eec7efe71695fc22bd8
lmonchon <lmonchon@LMONCHON>
2026-04-30 00:51:26 -0500

    Correciones

--- App_Bancaria_Backend/Repositories/Admin/AdminRepository.cs
+++ App_Bancaria_Backend/Repositories/Admin/AdminRepository.cs
@@ -17,7 +17,7 @@ namespace App_Bancaria_Backend.Repositories.Admin
         public async Task<List<UsuarioAdminDto>> GetUsuariosAsync(string? search)
         {
             var query = _context.Usuario
-                .Where(u => u.FlgEli != true && u.FlgEstado != true)
+                .Where(u => u.FlgEli == false && u.FlgEstado == false)
                 .Include(u => u.Cuenta)
                 .AsQueryable();
 
